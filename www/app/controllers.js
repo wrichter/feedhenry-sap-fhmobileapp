876d6edce7e1be5c3ed9ed047d98fec869f58af0
wrichter@redhat.com <wrichter@redhat.com>
2015-03-04 20:03:10 +0000

    update file /www/app/controllers.js

--- www/app/controllers.js
+++ www/app/controllers.js
@@ -86,7 +86,7 @@ myApp.controller('MainCtrl', function($scope, $q, fhcloud) {
          * One of these functions will be called when the $fh.cloud function has completed successully or encountered
          * an error.
          */
-        fhcloud.cloud('hello', userInput, defer.resolve, defer.reject);
+        fhcloud.cloud('create', userInput, defer.resolve, defer.reject);
       }
       
       
