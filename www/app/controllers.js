ea2d8b94cee37ffd6e0da29d9d8425ace86e0e28
wrichter@redhat.com <wrichter@redhat.com>
2015-03-04 20:02:53 +0000

    update file /www/app/controllers.js

--- www/app/controllers.js
+++ www/app/controllers.js
@@ -47,4 +47,48 @@ myApp.controller('MainCtrl', function($scope, $q, fhcloud) {
         fhcloud.cloud('hello', userInput, defer.resolve, defer.reject);
       }
     };
+    
+    
+    $scope.createCustomer = function() {
+      var custName = $scope.custName;
+      $scope.noticeMessage = "Calling Cloud Endpoint";
+      $scope.textClassName = "text-info";
+      var defer = $q.defer();
+      var promise = defer.promise;
+
+      //When the promise has completed, then the notice message can be updated to include result of the $fh.cloud call.
+      promise.then(function(response){
+        // If successful, display the length  of the string.
+        if (response && typeof response['@CUSTOMERNUMBER'] != 'undefined') {
+          if (response['@CUSTOMERNUMBER'] > 0) {
+            $scope.customerNumber = response['@CUSTOMERNUMBER'];
+            $scope.noticeMessage = undefined;
+            $scope.textClassName = "text-success";
+          } else {
+            scope.noticeMessage  = "Error: Customer not created. SAP returned:" +  JSON.stringify(response);
+            $scope.textClassName = "text-danger";
+          }
+        } else {
+          $scope.noticeMessage  = "Error: Expected a message from $fh.cloud.";
+          $scope.textClassName = "text-danger";
+        }
+      }, function(err){
+        //If the function
+        $scope.noticeMessage = "$fh.cloud failed. Error: " + JSON.stringify(err);
+      });
+
+      // check if userInput is defined
+      if (customerName) {
+        /**
+         * Pass the userInput to the module containing the $fh.cloud call.
+         *
+         * Notice that the defer.resolve and defer.reject functions are passed to the module.
+         * One of these functions will be called when the $fh.cloud function has completed successully or encountered
+         * an error.
+         */
+        fhcloud.cloud('hello', userInput, defer.resolve, defer.reject);
+      }
+      
+      
+    }
 });
